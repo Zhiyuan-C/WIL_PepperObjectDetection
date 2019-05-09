4a4a3bd8e4802c30d1bd87f78d0c8d0bd4334dca
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-09 16:07:57 +1000

    add object_detection func, initialise moveit and ros subscriber

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -11,11 +11,21 @@ from std_msgs.msg import String, Float32MultiArray
 import moveit_msgs.msg
 import moveit_commander
 from moveit_commander.conversions import pose_to_list
+# from control_msgs.msg import FollowJointTrajectoryGoal
 import geometry_msgs.msg
 from geometry_msgs.msg import Twist
 
+def object_detection(data):
+
+    pepper = moveit_commander.RobotCommander()
+    # move head
+    move_group = moveit_commander.MoveGroupCommander("head")
+    joint_goal = move_group.get_current_joint_values()
+    
 def main():
-    pass
+    # initialise ros node
+    rospy.init_node("object_detection", anonymous=True)
+    rospy.Subscriber("object", Float32MultiArray, object_detection, queue_size=1)
 
 if __name__ == '__main__':
     main()
