f891cccdb5f14c794f06bdf9d82bb6a1649efd54
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-09 15:21:41 +1000

    add lines for import lib

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python
 
+# import the basic libary
+import rospy
+import sys
+import copy
+from math import pi
+from std_msgs.msg import String, Float32MultiArray
+
+# import for moveit package
+import moveit_msgs.msg
+import moveit_commander
+from moveit_commander.conversions import pose_to_list
+import geometry_msgs.msg
+from geometry_msgs.msg import Twist
