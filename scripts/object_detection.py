8078078aed437c25851e7b7b9e0e771d9687ab18
kate <zhiyuan.chen@griffithuni.edu.au>
2019-05-09 22:20:46 +1000

    add motion_plan function, move moveit lines into the func

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -16,17 +16,18 @@ import geometry_msgs.msg
 from geometry_msgs.msg import Twist
 
 # https://github.com/ros/ros_comm/blob/ebd9e491e71947889eb81089306698775ab5d2a2/test/test_rospy/test/unit/test_rospy_topics.py
-def object_detection(data):
-    pepper = moveit_commander.RobotCommander()
-    # move head
-    move_group = moveit_commander.MoveGroupCommander("head")
-    joint_goal = move_group.get_current_joint_values()
-
+def object_recognition(data):
     # if no object detected
     if data.data.count() == 0:
         pass
     elif data.data.count() > 0:
         object_id = data.data[0]
+
+def motion_plan():
+    pepper = moveit_commander.RobotCommander()
+    # move head
+    move_group = moveit_commander.MoveGroupCommander("head")
+    joint_goal = move_group.get_current_joint_values()
         
     
 def main():
