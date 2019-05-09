9452f9f70fbf1e29771c1271435ebfbdd1f95564
kate <zhiyuan.chen@griffithuni.edu.au>
2019-05-09 22:34:05 +1000

    add more lines to the first test

--- scripts/test_object_detection.py
+++ scripts/test_object_detection.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 
 import unittest
+import object_detection
+from object_detection import object_recognition
+
+class TestObjectDetection(unittest.TestCase):
+    def test_image_recognition(self):
+        # test if outputs data
+        data = {data:[]} # put the data in
+        object_recognition(data)
