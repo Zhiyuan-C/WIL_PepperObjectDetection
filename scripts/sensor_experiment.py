e1b0dfd9ed55966557ee8b4e12b93eff1b0b1e9a
kate <zhiyuan.chen@griffithuni.edu.au>
2019-05-13 21:21:08 +1000

    create callback func

--- scripts/sensor_experiment.py
+++ scripts/sensor_experiment.py
@@ -4,3 +4,10 @@ import rospy
 from sensor_msgs import LaserScan # check with pepper, rostopic type /pepper_robot/laser
 from geometry_msgs import Twist
 
+def call_back(msg):
+    print(msg.ranges[60])
+    move.linear.x = 0.1
+    if msg.ranges[60] < 1:
+        move.linear.x = 0
+    pub.publish(move)
+
