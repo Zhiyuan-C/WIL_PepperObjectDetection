1448178fa735eb5a2beb5de447c45485011b1580
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-22 14:09:11 +1000

    modified the value for stop

--- scripts/sensor_experiment.py
+++ scripts/sensor_experiment.py
@@ -12,7 +12,7 @@ from geometry_msgs.msg import Twist
 def call_back(msg):
     print("middle: {} || sum: {} || avg: {}".format(msg.ranges[7], sum(msg.ranges), (sum(msg.ranges)/15))) # ranges have 15 messages in total, middle one should be 7, recheck with the srd_front/scan
     move.linear.x = 0.1
-    if msg.ranges[7] < 1: # should stop move forward if the middle value is less than 4, check first with actual no move
+    if msg.ranges[7] < 0.5: # should stop move forward if the middle value is less than 4, check first with actual no move
         move.linear.x = 0
     pub.publish(move)
 
