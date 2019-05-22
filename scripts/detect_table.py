e03c51c35ee824779607d78bfd72b224b04e4b89
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-22 10:23:46 +1000

    modified move_head and one_side func

--- scripts/detect_table.py
+++ scripts/detect_table.py
@@ -9,6 +9,13 @@ from geometry_msgs.msg import Twist
 import moveit_commander
 import moveit_msgs.msg
 from moveit_commander.conversions import pose_to_list
+
+class Error(Exception):
+    def __init__(self, error_message):
+        self.error_message = error_message
+    def ___str__(self):
+        return repr(self.error_message)
+
 # spin pepper to detect table
 class DetectTable(object):
     def __init__(self):
@@ -19,7 +26,7 @@ class DetectTable(object):
         # pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         # pub_msg = rospy.Publisher('detect/table/result', String, queue_size=10)
 
-        rospy.Rate = (10)
+        rate = rospy.Rate(10)
         
         rospy.loginfo("Start detecting table")
 
@@ -39,11 +46,12 @@ class DetectTable(object):
         self.at_right = False
         self.at_center = False
         self.detect_object = False
-        self.checked = False
+        self.one_side_check = False
         self.one_side_check_count = 5
-        rospy.spin() # testing purpose, delete this after
-
-        
+        self.cb_count = 0
+        while not rospy.is_shutdown():
+            self.move_head()
+            rate.sleep 
         # initialise for turning
         # self.spin_pepper = Twist()
         # self.start_spin = False
@@ -61,82 +69,52 @@ class DetectTable(object):
 
         #     rate.sleep()
         
-    def move_head(self):
-        self.joint_goal[0] = 0.5
-        rospy.loginfo(self.joint_goal)
-        
+    def one_side(self):
+        rospy.loginfo("current object detect => %s " % self.detect_object)
+        self.cb_count += 1
+        rospy.loginfo("call back count => %s " % self.cb_count)
         if self.one_side_check_count == 5 and not self.detect_object:
             rospy.loginfo("====Start initial position checking====")
-            rospy.loginfo("Current => %s" % self.joint_goal)
-            self.joint_goal[0] = 0.0
-            self.joint_goal[1] = 0.5
-            # self.execute_joint_goal()
+            move_head(self, 0.0, 0.5)
             self.one_side_check_count -= 1
-            rospy.loginfo("Sleep for 3 sec")
-            time.sleep(3)
         elif self.one_side_check_count == 4 and not self.detect_object:
             rospy.loginfo("====Start left checking 1====")
-            self.move_to_left()
-            # self.execute_joint_goal()
+            move_head(self, 0.5, 0.5)
             self.one_side_check_count -= 1
-            rospy.loginfo("Sleep for 3 sec")
         elif self.one_side_check_count == 3 and not self.detect_object:
             rospy.loginfo("====Start left checking 2====")
-            self.move_to_left()
+            move_head(self, 1.0, 0.5)
             # self.execute_joint_goal()
             self.one_side_check_count -= 1
-            rospy.loginfo("Sleep for 3 sec")
         elif self.one_side_check_count == 2 and not self.detect_object:
             rospy.loginfo("====Start right checking 1====")
-            self.joint_goal[0] = -0.5
-            self.joint_goal[1] = 0.5
-            # self.execute_joint_goal()
+            move_head(self, -0.5, 0.5)
             self.one_side_check_count -= 1
         elif self.one_side_check_count == 1 and not self.detect_object:
             rospy.loginfo("====Start right checking 2====")
-            self.move_to_right
-            # self.execute_joint_goal()
+            move_head(self, -1.0, 0.5)
             self.one_side_check_count -= 1
         elif self.one_side_check_count == 0 and not self.detect_object:
             rospy.loginfo("====No object detetect, one side check finish====")
-        
+            self.one_side_check = True
+        else:
+            rospy.loginfo("object detected")
+    
 
-            
-
-            
-        # pitch_val = 0.5
-        # if not self.detect_object and self.initial_pos:
-        #     rospy.loginfo(self.detect_object)
-        #     # set initial pose
-        #     rospy.loginfo("initialise first position")
-        #     self.move_center(pitch_val)
-        #     self.move_left = True
-        #     self.initial_pos = False
-        #     self.count += 1
-        #     rospy.loginfo("initial => %s" % self.count)
-        # elif self.move_left and not self.detect_object:
-        #     if self.joint_goal[0] <= 1:
-        #         self.move_to_left()
-        #         self.count += 1
-        #         rospy.loginfo("move left => %s" % self.count)
-        # elif self.move_right and not self.detect_object:
-        #     if -1 <= self.joint_goal[0]:
-        #         self.move_to_right()
-        #         self.count += 1
-        #         rospy.loginfo("move right => %s" % self.count) 
-        # else:
-        #     rospy.loginfo("finish move %s" % self.detect_object)
-
-    def move_center(self, pitch_val):
-        if -0.5 <= self.joint_goal[1] <= 0.5:
-            self.joint_goal[0] = 0.0
-            self.joint_goal[1] -= 0.5 # move down
-            # when at 0.5, the max left and right are 1 , -1
+    def move_head(self, pitch_val_a, pitch_val_b):
+        if -0.6 <= self.joint_goal[1] <= 0.6 and if -1.5 <= self.joint_goal[0] <= 1.5:
+            self.joint_goal[0] = pitch_val_a
+            self.joint_goal[1] = pitch_val_b # move down
             # self.execute_joint_goal()
+            # when at 0.5, the max left and right are 1 , -1
             # rospy.loginfo(move_group.get_current_state())
-
+            rospy.loginfo("moved to => %s" % self.joint_goal)
+            rospy.loginfo("Sleep for 3 sec")
             time.sleep(3)
-            rospy.loginfo("initial joint => %s" % self.joint_goal)
+        else:
+            raise Error("Joint value out of range")
+
+            
 
     def move_to_left(self):
         self.joint_goal[0] += 0.5
@@ -154,8 +132,6 @@ class DetectTable(object):
         self.joint_goal[0] -= 0.5
         # self.execute_joint_goal(joint_goal)
         rospy.loginfo("New => %s" % self.joint_goal)
-        if self.joint_goal[0] < -1:
-            self.move_right = False
         rospy.loginfo("Sleep for 3 sec")
         time.sleep(3)
 
@@ -179,7 +155,7 @@ class DetectTable(object):
             self.detect_object = True
         else:
             self.detect_object = False
-        self.move_head()
+        # self.move_head()
         
         # # no object detect
         # if len(objects.data) == 0:
@@ -252,3 +228,4 @@ class DetectTable(object):
 
 if __name__ == '__main__':
     detect = DetectTable()
+    
