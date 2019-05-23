f14f2699a345c6aedf349ccf40ada8c3f605a4cb
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-23 11:28:39 +1000

    add line to publish string

--- scripts/detect_table.py
+++ scripts/detect_table.py
@@ -73,6 +73,9 @@ class DetectTable(object):
                     rospy.loginfo("The object is in front of pepper!")
                     # move close to the table object
                 elif self.at_left:
+                    if not self.detect_object:
+                        pub_msg.publish("left")
+
                     # if not self.done_turning:
                     #     rospy.loginfo("The object is at left side of pepper, turn left")
                     #     if not self.finish_spin:
@@ -91,6 +94,8 @@ class DetectTable(object):
                     #     rospy.loginfo("Turned, object is infront")
 
                 elif self.at_right:
+                    if not self.detect_object:
+                        pub_msg.publish("right")
                     # if not self.done_turning:
                     #     rospy.loginfo("The object is at left side of pepper, turn left")
                     #     if not self.finish_spin:
