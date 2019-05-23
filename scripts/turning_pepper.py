8081d47da17caa26787525868bce47614575c592
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-23 12:18:45 +1000

    initialise empty funcs

--- scripts/turning_pepper.py
+++ scripts/turning_pepper.py
@@ -3,3 +3,15 @@
 import rospy
 from std_msgs.msg import String, Float32MultiArray
 from geometry_msgs.msg import Twist
+
+def get_objects(objects):
+    pass
+
+def get_direction(msg):
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
