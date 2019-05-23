aa37601dff5c8a24c771d10896ea4fca09f0c18f
kate <zhiyuan.chen@griffithuni.edu.au>
2019-05-23 20:53:20 +1000

    add docstring and comments, remove white spaces

--- scripts/turning_pepper.py
+++ scripts/turning_pepper.py
@@ -9,38 +9,37 @@ import numpy
 import cv2
 
 class TurningPepper(object):
-
+    """ Turning Pepper towards object """
     def __init__(self):
+        """ Initialise class, and looping the code untill shutdown the node """
+        # initialise
         rospy.init_node("turning_pepper", anonymous=True)
         rospy.Subscriber("/objects", Float32MultiArray, self.get_object_center)
         rospy.Subscriber('detect_table_result', String, self.get_direction)
-
+        # initialise publisher
         self.pub_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         pub_msg = rospy.Publisher('approach_table', String, queue_size=10)
-
+        # initialise for determine the center of the object
         self.object_center = None
         self.go_right = False
         self.go_left = False
         self.detect_object = False
         self.ready_to_spin = False
-
-        
+        # initialise for turning the pepper
         self.spin_pepper = Twist()
         self.approach = False
         rate = rospy.Rate(10)
-
+        # start node with call back functions and publish new topics
         while not rospy.is_shutdown():
             if self.ready_to_spin:
                 if not self.approach:
                     self.turning_pepper()
                 elif self.approach:
                     pub_msg.publish("ready")
-
-
             rate.sleep()
 
-
     def get_object_center(self, objects):
+        """ Detect the coordinate for the center of the object """
         if len(objects.data) > 0 and objects.data[0] == 1:
             self.detect_object = True
             # get transformation matrix
@@ -67,6 +66,7 @@ class TurningPepper(object):
             self.object_center = (outpt_array[0, 0] + outpt_array[1, 0] + outpt_array[2, 0] + outpt_array[3, 0]) / 4
 
     def get_direction(self, msg):
+        """ Determine the object is at which side of pepper """
         if len(msg.data) > 0:
             self.ready_to_spin = True
         if msg.data == "right":
@@ -75,10 +75,12 @@ class TurningPepper(object):
             self.go_left = True
     
     def turning_pepper(self):
+        """ Truning pepper untill the object center (x-axis) is at the frame center (x-axis) """
         if self.detect_object and 158 < self.object_center[0] < 162:
+            # if the object center is at frame center, then stop turning
             self.spin_pepper.angular.z = 0.0
             self.pub_vel.publish(self.spin_pepper)
-            time.sleep(5)
+            time.sleep(5) # publish message only for 5 seconds
             self.approach = True
         elif self.go_right:
             self.spin_pepper.angular.z = -0.1
@@ -86,10 +88,6 @@ class TurningPepper(object):
         elif self.go_left:
             self.spin_pepper.angular.z = 0.1
             self.pub_vel.publish(self.spin_pepper)
-
-    
-
-
 
 if __name__ == "__main__":
     start = TurningPepper()
