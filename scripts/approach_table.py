b50d0f664781c600408013941f6a1d8b0e3f6c21
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-23 13:52:46 +1000

    create call back funcs

--- scripts/approach_table.py
+++ scripts/approach_table.py
@@ -7,10 +7,22 @@ from sensor_msgs.msg import LaserScan
 
 class ApproachTable(object):
     def __init__(self):
+        
+        rospy.init_node("approach_table", anonymous=True)
+        rospy.Subscriber('/laser/srd_front/scan', LaserScan, get_laser_msg)
+        rospy.Subscriber('approach_table', String, self.get_approach_info)
+        pub = rospy.Publisher('cmd_vel', Twist)
+
+        self.approach = False
         self.move = Twist()
 
 
-    def call_back(msg):
+
+    def get_laser_msg(msg):
         self.move.linear.x = 0.1
         if msg.ranges[7] < 0.45: # should stop move forward if the middle value is less than 4, check first with actual no move
-            move.linear.x = 0
+            self.move.linear.x = 0
+    
+    def get_approach_info(msg):
+        if len(msg.data) > 0 and msg.data == "ready":
+            self.approach = True
