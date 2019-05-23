7a8c12b4f926926a81eadfadace0f920f2e87173
Zhiyuan Chen <zhiyuan.chen@griffithuni.edu.au>
2019-05-23 14:31:46 +1000

    fixed syntax error

--- scripts/approach_table.py
+++ scripts/approach_table.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import time
+
 import rospy
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
@@ -8,36 +10,35 @@ from sensor_msgs.msg import LaserScan
 class ApproachTable(object):
     def __init__(self):
         
-        rospy.init_node("approach_table", anonymous=True)
-        rospy.Subscriber('/laser/srd_front/scan', LaserScan, get_laser_msg)
+        rospy.init_node("approach", anonymous=True)
+        rospy.Subscriber('/laser/srd_front/scan', LaserScan, self.get_laser_msg)
         rospy.Subscriber('approach_table', String, self.get_approach_info)
-        pub = rospy.Publisher('cmd_vel', Twist)
+        pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
         self.approach = False
         self.move = Twist()
-        self.close = False
+        self.close_to = False
 
         while not rospy.is_shutdown():
             if self.approach:
+                rospy.loginfo("approach at %s" % self.move)
                 pub.publish(self.move)
-                if self.close:
-                    sleep(5)
+                if self.close_to:
+                    time.sleep(5)
                     break
 
-
-
-    def get_laser_msg(msg):
+    def get_laser_msg(self, msg):
         
-        if msg.ranges[7] < 0.45: # should stop move forward if the middle value is less than 4, check first with actual no move
-            self.close = True
+        if msg.ranges[7] <= 0.5: # should stop move forward if the middle value is less than 4, check first with actual no move
+            self.close_to = True
             self.move.linear.x = 0.0
-        elif msg.ranges[7] > 0.45:
+        elif msg.ranges[7] >= 0.5:
             self.move.linear.x = 0.1
     
-    def get_approach_info(msg):
+    def get_approach_info(self, msg):
         if len(msg.data) > 0 and msg.data == "ready":
             self.approach = True
 
 
 if __name__ == "__main__":
-    start = TurningPepper()
+    start = ApproachTable()
